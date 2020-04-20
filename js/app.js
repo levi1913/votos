86c2127f88dd23c4f33fe884a0bc39a2647c41bd
levi1913 <58019791+levi1913@users.noreply.github.com>
2020-04-19 21:20:14 -0500

    levi

--- js/app.js
+++ js/app.js
@@ -4,5 +4,9 @@ $(document).ready(function(){
         $("#sigueme").css("left",event.clientX-50)
                      .css("top",event.clientY-50);
     });
+    $(document).on("touchmove",function(){
+        $("#sigueme").css("left",event.clientX-50)
+                     .css("top",event.clientY-50);
+    });
 
 });
